db0a08c8721747bdcaa740681fadff45adaccbf1
RDS5CLJ <rds5clj@bosch.com>
2022-12-04 19:04:16 +0200

    more cors :(

--- DS2022_30442_Rusu_David_1_Backend/src/main.ts
+++ DS2022_30442_Rusu_David_1_Backend/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
-import cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   
   app.useGlobalPipes(new ValidationPipe());
 
