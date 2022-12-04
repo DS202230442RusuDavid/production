469f059aa749c5925b62c5dec7b78c23c312a4a6
RDS5CLJ <rds5clj@bosch.com>
2022-12-04 17:48:03 +0200

    remopved cors

--- DS2022_30442_Rusu_David_1_Backend/src/main.ts
+++ DS2022_30442_Rusu_David_1_Backend/src/main.ts
@@ -6,17 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-});
 
-app.enableCors({
-    allowedHeaders:"*",
-    origin: "*"
-});
   app.use(cookieParser());
   await app.listen(3000);
 }
