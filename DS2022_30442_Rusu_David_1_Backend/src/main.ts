af9647058fdc460a05c9be2b6bec7a1624267319
RDS5CLJ <rds5clj@bosch.com>
2022-12-04 19:16:02 +0200

    corssssss

--- DS2022_30442_Rusu_David_1_Backend/src/main.ts
+++ DS2022_30442_Rusu_David_1_Backend/src/main.ts
@@ -7,11 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: false,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.use(cookieParser());
