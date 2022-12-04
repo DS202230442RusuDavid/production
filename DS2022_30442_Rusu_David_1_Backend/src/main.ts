277868faaf693b32a1457cb03027223bade4075f
RDS5CLJ <rds5clj@bosch.com>
2022-12-04 22:21:25 +0200

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- DS2022_30442_Rusu_David_1_Backend/src/main.ts
+++ DS2022_30442_Rusu_David_1_Backend/src/main.ts
@@ -6,11 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: "*",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: "*",
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   preflightContinue: false,
+  //   credentials: true,
+  // });
 
   app.useGlobalPipes(new ValidationPipe());
 
