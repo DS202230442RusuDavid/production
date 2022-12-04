8d0d6db8ee17780cd004cc523a428c49c298c39e
RDS5CLJ <rds5clj@bosch.com>
2022-12-04 18:09:15 +0200

    added cors module

--- DS2022_30442_Rusu_David_1_Backend/src/main.ts
+++ DS2022_30442_Rusu_David_1_Backend/src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions ={
+    origin:'*', 
+    credentials:true, //access-control-allow-credentials:true
+     optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
   app.useGlobalPipes(new ValidationPipe());
 
   app.use(cookieParser());
