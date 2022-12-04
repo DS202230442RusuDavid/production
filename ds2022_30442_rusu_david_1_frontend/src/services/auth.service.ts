bb44820d801a8eb2ecd0a7bbdb91a2020fd3c2a2
RDS5CLJ <rds5clj@bosch.com>
2022-12-04 16:45:51 +0200

    remove reverse proxy, hardcodedmip

--- ds2022_30442_rusu_david_1_frontend/src/services/auth.service.ts
+++ ds2022_30442_rusu_david_1_frontend/src/services/auth.service.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 import Role from "../dtos/role.dto";
 import User from "../dtos/user.dto";
 
+const ip = process.env.backendIP || "http://localhost:3000";
+
 export const register = async (email: string, password: string, role: Role) => {
     console.log({ email, password, role });
     try {
-        const response = await axios.post("/authentication/register", {
+        const response = await axios.post(ip + "/authentication/register", {
             email,
             password,
             role,
@@ -23,7 +25,7 @@ export const login = async (
     ) => {
     try {
         const res = await axios.post(
-            "/authentication/log-in",
+            ip + "/authentication/log-in",
             { email, password },
             { withCredentials: true }
         );
