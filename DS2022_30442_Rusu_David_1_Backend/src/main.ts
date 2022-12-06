5ab2df3a6a0520d3f7b6d530b64ccd4cb54c68a8
RDS5CLJ <rds5clj@bosch.com>
2022-12-06 16:45:52 +0200

    cred false

--- DS2022_30442_Rusu_David_1_Backend/src/main.ts
+++ DS2022_30442_Rusu_David_1_Backend/src/main.ts
@@ -2,24 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
  
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  
-
-  app.enableCors({
-    // allowedHeaders:"*",
-    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  app.use(cors({
     origin: ['http://int32.duckdns.org:4000/'],
-    credentials: true,
-    // preflightContinue: true
-  });
-
+    // credentials: true,
+  }));
   
-
   await app.listen(3000);
 }
 bootstrap();
