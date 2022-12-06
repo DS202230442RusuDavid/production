d5a138cd95c4912e17b7723829f84d7f1653f536
RDS5CLJ <rds5clj@bosch.com>
2022-12-06 21:32:01 +0200

    fix origin

--- DS2022_30442_Rusu_David_1_Backend/src/main.ts
+++ DS2022_30442_Rusu_David_1_Backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-    origin: 'http://int32.duckdns.org:4000/',
+    origin: 'http://int32.duckdns.org:4000',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     preflightContinue: false,
